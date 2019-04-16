5c341d5525c4471c1c38c03b287d5af3204175b7
Quentin Danneville <quentin@nextmap.io>
2019-04-16 08:51:01 +0200

    [Initial-commit] User authenticate added

---
+++ _helpers/db.js
@@ -0,0 +1,40 @@
+// const config = require("config.json");
+const mysql = require("mysql");
+
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+// mongoose.Promise = global.Promise;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "qdan",
+  password: "derpderp",
+  database: "app"
+});
+
+// connect to database
+db.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
+});
+
+global.db = db;
+
+module.exports = db;
+
+// Exemple de requête
+// let query = "SELECT * FROM `user`";
+
+// db.query(query, (err, result) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// module.exports = {
+//     User: require('../users/user.model')
+// };
---
+++ users/user.service.js
@@ -0,0 +1,28 @@
+﻿const config = require("config.json");
+const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+const db = require("_helpers/db");
+
+module.exports = {
+  authenticate
+};
+
+async function authenticate({ username, password }) {
+  var sql =
+    "SELECT username, hash, firstname, lastname, createdDate FROM `user` WHERE `username`='" +
+    username +
+    "' and hash = '" +
+    password +
+    "'";
+
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        let user = results[0];
+        resolve(user);
+      }
+    });
+  });
+}
