2209bfb415a953f6cf0fc81d0f87fca8604c1e83
Quentin Danneville <quentin@nextmap.io>
2019-04-16 21:15:19 +0200

    [Users] Added all controller and services
    Register
    getAll
    getCurrent
    /:id
    /:id _delete

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -6,7 +6,7 @@ module.exports = jwt;
 
 function jwt() {
   const secret = config.secret;
-  return expressJwt({ secret, isRevoked }).unless({
+  return expressJwt({ secret }).unless({
     path: [
       // public routes that don't require authentication
       "/users/authenticate",
@@ -15,13 +15,14 @@ function jwt() {
   });
 }
 
-async function isRevoked(req, payload, done) {
-  const user = await userService.getById(payload.sub);
+// async function isRevoked(req, payload, done) {
+//   console.log(req);
+//   // const user = await userService.getById(payload.sub);
 
-  // revoke token if user no longer exists
-  if (!user) {
-    return done(null, true);
-  }
+//   // revoke token if user no longer exists
+//   // if (!user) {
+//   //   return done(null, true);
+//   // }
 
-  done();
-}
+//   // done();
+// }
--- users/user.service.js
+++ users/user.service.js
@@ -4,14 +4,19 @@ const jwt = require("jsonwebtoken");
 const db = require("_helpers/db");
 
 module.exports = {
-  authenticate
+  authenticate,
+  getAll,
+  getById,
+  create,
+  update,
+  delete: _delete
 };
 
 async function authenticate({ username, password }) {
   var sql =
-    "SELECT username, password, firstname, lastname, created_at FROM `user` WHERE `username`='" +
+    "SELECT username, hash, firstname, lastname, createdDate FROM `user` WHERE `username`='" +
     username +
-    "' and password = '" +
+    "' and hash = '" +
     password +
     "'";
 
@@ -19,9 +24,120 @@ async function authenticate({ username, password }) {
     db.query(sql, (err, results) => {
       if (err) {
         reject(err);
-      } else {
+      } else if (results.length) {
         let user = results[0];
-        resolve(user);
+        const token = jwt.sign({ sub: user.id }, config.secret);
+
+        resolve({
+          ...user,
+          token
+        });
+      } else {
+        resolve(results[0]);
+      }
+    });
+  });
+}
+
+async function getAll() {
+  let getAllUserSqlQuery = "SELECT * FROM `USER`";
+
+  return new Promise((resolve, reject) => {
+    db.query(getAllUserSqlQuery, (err, results) => {
+      if (results.length) {
+        return resolve(results);
+      } else {
+        return reject(err);
+      }
+    });
+  });
+}
+
+async function getById(id) {
+  var sql =
+    "SELECT id, username, hash, firstname, lastname FROM `user` WHERE `id`=" +
+    id;
+
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, results) => {
+      if (results.length) {
+        return resolve(results[0]);
+      } else {
+        return reject("User is not found");
+      }
+    });
+  });
+}
+
+async function create(userParam) {
+  let username = userParam.username;
+  let password = userParam.password;
+  let firstname = userParam.firstname;
+  let lastname = userParam.lastname;
+
+  var checkUserSqlQuery =
+    "SELECT username, hash, firstname, lastname, createdDate FROM `user` WHERE `username`='" +
+    username +
+    "'";
+
+  var createUserSqlQuery =
+    "INSERT INTO `user` (`username`, `hash`, `firstname`, `lastname`) VALUES ('" +
+    username +
+    "', '" +
+    password +
+    "', '" +
+    firstname +
+    "', '" +
+    lastname +
+    "')";
+
+  return new Promise((resolve, reject) => {
+    db.query(checkUserSqlQuery, (err, results) => {
+      if (results.length) {
+        return reject("Username already exists");
+      } else {
+        db.query(createUserSqlQuery, (err, results) => {
+          if (err) {
+            return reject(err);
+          } else {
+            return resolve();
+          }
+        });
+      }
+    });
+  });
+}
+
+async function update(id, userParam) {
+  let updateUserSqlQuery =
+    "UPDATE user SET username = '" + userParam.username + "' WHERE id = " + id;
+
+  return new Promise((resolve, reject) => {
+    getById(id)
+      .then(() => {
+        db.query(updateUserSqlQuery, (err, results) => {
+          if (err) {
+            return reject(err);
+          } else {
+            return resolve();
+          }
+        });
+      })
+      .catch(err => {
+        return reject(err);
+      });
+  });
+}
+
+async function _delete(id) {
+  let deleteUserSqlQuery = "DELETE FROM user WHERE id = " + id;
+
+  return new Promise((resolve, reject) => {
+    db.query(deleteUserSqlQuery, (err, results) => {
+      if (results.affectedRows) {
+        resolve();
+      } else {
+        reject("User doesn't exist");
       }
     });
   });
